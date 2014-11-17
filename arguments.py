18be82745d84e0838e2d8d722730e2aede1b3898
Pekka Klärck <peke@iki.fi>
2014-11-17 16:03:40 +0200

    move helper functions to helper methods, python2.6 compatibility

--- arguments.py
+++ arguments.py
@@ -8,57 +8,55 @@ BASE_TYPES = [int, long, bool, float, str, unicode]
 class arguments(object):
 
     def __init__(self, *arg_types, **kwarg_types):
-        validate_types(arg_types + tuple(kwarg_types.values()))
+        self._validate_types(arg_types + tuple(kwarg_types.values()))
         self.arg_types = arg_types
         self.kwarg_types = kwarg_types
+
+    def _validate_types(self, arg_types):
+        for arg_type in arg_types:
+            if not isinstance(arg_type, (type, ClassType, tuple, NoneType)):
+                raise TypeError('Argument type must be class, tuple of classes '
+                                'or None, got <%s> instance instead.'
+                                % type(arg_type).__name__)
 
     @decorator
     def __call__(self, wrapped, instance, args, kwargs):
         argspec = inspect.getargspec(wrapped).args
-        args = handle_args(args, argspec, self.arg_types, self.kwarg_types)
-        kwargs = dict(handle_kwargs(kwargs, self.kwarg_types))
+        args = self._handle_args(args, argspec)
+        kwargs = dict(self._handle_kwargs(kwargs))
         return wrapped(*args, **kwargs)
 
+    def _handle_args(self, args, argspec):
+        for index, arg in enumerate(args):
+            if index < len(self.arg_types):
+                arg = self._handle_arg(arg, self.arg_types[index], index+1)
+            elif argspec[index] in self.kwarg_types:
+                name = argspec[index]
+                arg = self._handle_arg(arg, self.kwarg_types[name], name)
+            yield arg
 
-def validate_types(arg_types):
-    for arg_type in arg_types:
-        if not isinstance(arg_type, (type, ClassType, tuple, NoneType)):
-            raise TypeError('Argument type must be class, tuple of classes or '
-                            'None, got <{}> instance instead.'.format(
-                            type(arg_type).__name__))
+    def _handle_kwargs(self, kwargs):
+        for name, arg in kwargs.iteritems():
+            if name in self.kwarg_types:
+                arg = self._handle_arg(arg, self.kwarg_types[name], name)
+            yield name, arg
 
-def handle_args(args, argspec, arg_types, kwarg_types):
-    for index, arg in enumerate(args):
-        if index < len(arg_types):
-            arg = verify_argument(arg, arg_types[index], index+1)
-        elif argspec[index] in kwarg_types:
-            name = argspec[index]
-            arg = verify_argument(arg, kwarg_types[name], name)
-        yield arg
+    def _handle_arg(self, argument, expected_type, label):
+        if expected_type is None or isinstance(argument, expected_type):
+            return argument
+        if expected_type in BASE_TYPES:
+            try:
+                return expected_type(argument)
+            except TypeError:
+                pass
+        raise RuntimeError('Argument %s should have been %s but was <%s>.'
+                           % (label, self._format_expected(expected_type),
+                              type(argument).__name__))
 
-def handle_kwargs(kwargs, kwarg_types):
-    for name, arg in kwargs.iteritems():
-        if name in kwarg_types:
-            arg = verify_argument(arg, kwarg_types[name], name)
-        yield name, arg
-
-
-def verify_argument(argument, expected_type, label):
-    if expected_type is None or isinstance(argument, expected_type):
-        return argument
-    if expected_type in BASE_TYPES:
-        try:
-            return expected_type(argument)
-        except TypeError:
-            pass
-    raise RuntimeError('Argument {} should have been {} but was <{}>.'.format(
-        label, _format_expected(expected_type), type(argument).__name__))
-
-
-def _format_expected(expected):
-    if not isinstance(expected, tuple):
-        expected = (expected,)
-    expected = ['<{}>'.format(exp.__name__) for exp in expected]
-    if len(expected) == 1:
-        return expected[0]
-    return '{} or {}'.format(', '.join(expected[:-1]), expected[-1])
+    def _format_expected(self, expected):
+        if not isinstance(expected, tuple):
+            expected = (expected,)
+        expected = ['<%s>' % exp.__name__ for exp in expected]
+        if len(expected) == 1:
+            return expected[0]
+        return '%s or %s' % (', '.join(expected[:-1]), expected[-1])
